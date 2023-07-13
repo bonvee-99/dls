64a267a5cf4be950837f6b275eddb3ae7796ebe7
Ben Vinnick <bvinnick@thinkwellgroup.com>
2023-07-13 12:11:13 -0700

    setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,8 @@
-import express from 'express';
-import http from 'http';
-import WebSocket from 'ws';
-import { AddressInfo } from 'net';
+import express from 'express'
+import http from 'http'
+import WebSocket from 'ws'
+import { AddressInfo } from 'net'
+import { v4 as uuidv4 } from 'uuid'
 
 const app = express();
 // initialize a simple http server
@@ -9,17 +10,41 @@ const server = http.createServer(app);
 // initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+interface Custom_Web_Socket extends WebSocket {
+  id: string,
+  rooms: []
+}
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+interface WS_Message {
+  type: string
+}
+
+wss.on('connection', function connection(ws: Custom_Web_Socket) {
+  ws.id = uuidv4()
+  console.log('New client connected: ', ws.id)
+
+  ws.on('close', () => {
+    console.log(`Client ${ws.id} disconnected`)
+  })
+
+  ws.on('message', (data: string) => {
+    try {
+      console.log(`Client ${ws.id} has sent us: ${data}`)
+      const parsedMessage: WS_Message = JSON.parse(data)
+    } catch (error) {
+      console.error(error)
+      ws.send('Received invalid JSON')
+    }
+  })
+
+  ws.send(`Your id is: ${ws.id}`)
+
+  wss.clients.forEach((client) => {
+    console.log('Client.ID: ', (client as Custom_Web_Socket).id)
   });
-
-  ws.send('something');
-});
+})
 
 // start our server
-server.listen(process.env.PORT || 8999, () => {
-    console.log(`Server started on port ${(server.address() as AddressInfo).port} :)`);
-});
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port ${(server.address() as AddressInfo).port}`)
+})
